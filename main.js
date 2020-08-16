d3549455585e393b6b17f709aa8d269c4866ad67
Andrew Brainerd <drwb333@gmail.com>
2020-08-16 14:28:51 -0400

    Add Electron

--- main.js
+++ main.js
@@ -1,26 +1,45 @@
+const { app, BrowserWindow } = require('electron');
 const chokidar = require('chokidar');
 const { default: SlippiGame } = require('@slippi/slippi-js');
 const { gameCompleted } = require('./api');
 
-const watcher = chokidar.watch('C:/SSBReplays', {
-  depth: 0,
-  persistent: true,
-  usePolling: true,
-  ignoreInitial: true,
-}).on('all', (event, path) => {
-  event !== 'change' && console.log(event, path);
-});
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
 
-watcher.on('change', path => {
-  let game = new SlippiGame(path, { processOnTheFly: true });
-  const metadata = game.getMetadata();
+  win.loadFile('index.html');
+};
 
-  if (metadata) {
-    console.log('Game Finished', metadata);
+app.whenReady().then(() => {
+  createWindow();
 
-    const settings = game.getSettings();
-    const stats = game.getStats();
+  const watcher = chokidar.watch('C:/SSBReplays', {
+    depth: 0,
+    persistent: true,
+    usePolling: true,
+    ignoreInitial: true,
+  })
 
-    gameCompleted({ user: 'Sp1d3rM0nk3y', metadata, settings, stats });
-  }
+  watcher.on('add', path => {
+    console.log('New Game Started');
+  });
+  
+  watcher.on('change', path => {
+    let game = new SlippiGame(path, { processOnTheFly: true });
+    const metadata = game.getMetadata();
+  
+    if (metadata) {
+      console.log('Game Finished', metadata);
+  
+      const settings = game.getSettings();
+      const stats = game.getStats();
+  
+      gameCompleted({ user: 'Sp1d3rM0nk3y', metadata, settings, stats });
+    }
+  });
 });
