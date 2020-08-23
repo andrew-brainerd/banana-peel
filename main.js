10278d38469a9f8abbbcefe48c65d351179d5c03
Andrew Brainerd <drwb333@gmail.com>
2020-08-23 12:14:59 -0400

    Update window position

--- main.js
+++ main.js
@@ -1,22 +1,31 @@
 require('dotenv').config()
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain, screen } = require('electron');
 const { initializeWatcher } = require('./src/replayWatcher');
 
 const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 335,
-    height: 450,
-    minWidth: 335,
-    minHeight: 450,
-    maxWidth: 335,
-    maxHeight: 450,
+  const primaryDisplay = screen.getPrimaryDisplay();
+  const windowWidth = 335;
+  const windowHeight = 450;
+  const xPosition = primaryDisplay.bounds.width - windowWidth;
+  const yPosition = primaryDisplay.bounds.height - windowHeight;
+
+  const mainWindow = new BrowserWindow({
+    frame: true,
+    title: 'Banana Peel',
+    width: windowWidth,
+    height: windowHeight,
+    x: xPosition,
+    y: yPosition,
+    resizable: false,
+    autoHideMenuBar: true,
+    darkTheme: true,
     webPreferences: {
       nodeIntegration: true
     }
   });
 
-  win.loadFile('index.html');
-  win.minimize();
+  mainWindow.loadFile('index.html');
+  // mainWindow.minimize();
 };
 
 app.whenReady().then(() => {
