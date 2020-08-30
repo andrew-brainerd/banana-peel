6a316d28e45656c24c7d54ea59abcdb0b448d07f
Andrew Brainerd <drwb333@gmail.com>
2020-08-30 16:43:16 -0400

    Update README

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ const createWindow = () => {
     height: windowHeight,
     x: xPosition,
     y: yPosition,
-    // show: false,
+    show: false,
     resizable: false,
     autoHideMenuBar: true,
     darkTheme: true,
